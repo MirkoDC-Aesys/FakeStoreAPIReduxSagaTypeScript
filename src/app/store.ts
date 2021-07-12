6184fdaa1aac32a828107869af3085fdf5dbc8f8
MirkoDC-Aesys <85115982+MirkoDC-Aesys@users.noreply.github.com>
2021-07-12 17:44:04 +0200

    Add sagaMiddleware to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,18 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
+import createSagaMiddleware from 'redux-saga'
 import productsReducer from '../features/products/productsSlice'
+import rootSaga from '../sagas/rootSaga'
+
+const sagaMiddleware = createSagaMiddleware()
 
 export const store = configureStore({
   reducer: {
     counter: productsReducer,
-  }
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware)
 })
+
+sagaMiddleware.run(rootSaga)
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
