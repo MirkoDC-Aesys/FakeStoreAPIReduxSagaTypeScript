989645453a87471837d1a73e22395f2680fe7d87
MirkoDC-Aesys <85115982+MirkoDC-Aesys@users.noreply.github.com>
2021-07-12 18:02:55 +0200

    Add Router in App.ts.Add Products component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './App.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import Products from './features/products/Products'
 
 function App() {
   return (
     <div>
-      App
+      <h2>Shop</h2>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Products} />
+          <Route path={`/product/:id`} render={() => <div>Product</div>} />
+        </Switch>
+      </Router>
     </div>
   );
 }
